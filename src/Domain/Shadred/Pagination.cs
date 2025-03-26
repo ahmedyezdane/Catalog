bd42e1a0f558feddf86996d01bcc0d4175438c1c
Ahmad Yazdany <ahmadyazdany18@gmail.com>
2025-03-26 16:55:45 +0330

    feat: Result and Pagination Class Added

---
+++ src/Domain/Shadred/Pagination.cs
@@ -0,0 +1,56 @@
+﻿namespace Domain.Shadred;
+
+public class Pagination<TData>
+{
+    public int TotalItems { get; set; }
+    public int PageSize { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int StartPage { get; set; }
+    public int EndPage { get; set; }
+    public List<TData> Data { get; set; } = new();
+
+    public Pagination(List<TData> data, int totalItems, int pageNo, int pageSize)
+    {
+        Data = data;
+
+        int currentPage = pageNo;
+        int startPage = currentPage - 2;
+        int endPage = currentPage + 3;
+
+        if (pageSize <= 0)
+            pageSize = 20;
+        if (pageNo <= 0)
+            pageNo = 1;
+
+        int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+
+        if (totalPages <= 0)
+            totalPages = 1;
+
+        if (pageNo > totalPages)
+            pageNo = totalPages;
+
+        if (startPage <= 0)
+        {
+            endPage = endPage - (startPage - 1);
+            startPage = 1;
+        }
+        if (endPage > totalPages)
+        {
+            endPage = totalPages;
+            if (endPage > 10)
+            {
+                startPage = endPage - 4;
+            }
+        }
+
+        TotalItems = totalItems;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+        TotalPages = totalPages;
+        StartPage = startPage;
+        EndPage = endPage;
+    }
+}
+
---
+++ src/Domain/Shadred/Result.cs
@@ -0,0 +1,66 @@
+﻿namespace Domain.Shadred;
+
+public class Result
+{
+    public ResultTypes Type { get; init; }
+
+    public string Message { get; init; }
+
+    public bool IsSuccess => Type == ResultTypes.Success;
+    public bool IsFailure => Type == ResultTypes.Failure;
+
+    #region Factory
+
+    public static Result Failure(string message)
+    {
+        return new Result { Type = ResultTypes.Failure, Message = message };
+    }
+    public static Result Success(string message)
+    {
+        return new Result { Type = ResultTypes.Success, Message = message };
+    }
+
+    public static Result Failure()
+    {
+        return new Result { Type = ResultTypes.Failure, Message = "Failed to complete the operation" };
+    }
+    public static Result Success()
+    {
+        return new Result { Type = ResultTypes.Success, Message = "Operation completed successfully" };
+    }
+
+    #endregion
+}
+
+public class Result<TData> : Result
+{
+    public TData Data { get; set; }
+
+    #region Factory
+
+    public static Result<TData> Failure(string message, TData data)
+    {
+        return new Result<TData> { Type = ResultTypes.Failure, Message = message, Data = data };
+    }
+    public static Result<TData> Success(string message, TData data)
+    {
+        return new Result<TData> { Type = ResultTypes.Success, Message = message, Data = data };
+    }
+
+    public static Result<TData> Failure(TData data)
+    {
+        return new Result<TData> { Type = ResultTypes.Failure, Message = "Failed to complete the operation", Data = data };
+    }
+    public static Result<TData> Success(TData data)
+    {
+        return new Result<TData> { Type = ResultTypes.Success, Message = "Operation completed successfully", Data = data };
+    }
+
+    #endregion
+}
+
+public enum ResultTypes
+{
+    Success = 1,
+    Failure = 2,
+}
